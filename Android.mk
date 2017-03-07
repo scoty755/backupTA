011ccb047630543a6cc43be5b40e476fe90243b6
scoty755 <scoty755@gmail.com>
2017-03-07 22:28:51 +0900

    Add library

--- Android.mk
+++ Android.mk
@@ -18,7 +18,10 @@ include $(BUILD_EXECUTABLE)
 include $(CLEAR_VARS)
 LOCAL_MODULE := dumpta
 LOCAL_SRC_FILES := dumpta.c
-LOCAL_SHARED_LIBRARIES := liblog
+LOCAL_SHARED_LIBRARIES := liblog \
+	libutils\
+	libcutils
+LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -llog
 LOCAL_CFLAGS := -std=c99
 LOCAL_MULTILIB := both
 LOCAL_MODULE_STEM_32 := dumpta32
@@ -30,6 +33,7 @@ LOCAL_MODULE := cow-run-as
 LOCAL_SRC_FILES := \
 	run-as.c
 LOCAL_SHARED_LIBRARIES := liblog libselinux
+LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -llog
 LOCAL_CFLAGS += -std=c99 -Os
 LOCAL_CFLAGS += -fPIE
 LOCAL_LDFLAGS += -fPIE -pie
@@ -44,6 +48,7 @@ LOCAL_MODULE := exploitta
 LOCAL_SRC_FILES := \
 	exploitta.c dirtycow.c
 LOCAL_SHARED_LIBRARIES := liblog libselinux
+LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -llog
 LOCAL_CFLAGS += -std=c99 -DDEBUG
 LOCAL_CFLAGS += -fPIE
 LOCAL_LDFLAGS += -fPIE -pie
@@ -56,6 +61,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := checkta
 LOCAL_SRC_FILES := \
 	checkta.c
+LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -llog
 LOCAL_CFLAGS += -fPIE
 LOCAL_LDFLAGS += -fPIE -pie
 LOCAL_MULTILIB := both
